bd3607ac87e672a9a243cf49a121f10701723db7
Grimet14 <gileff.simon@gmail.com>
2023-05-25 00:20:38 -0700

    stuff still needs to be fixed

--- script.js
+++ script.js
@@ -1,63 +1,34 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+let expression = '';
 
-// Function to evaluate an arithmetic expression
-function evaluateExpression(expression) {
-  try {
-    const result = new Function(`return ${expression}`)();
-    return result;
-  } catch (error) {
-    console.error(error);
-    return "Error: " + error.message;
-  }
-}
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.innerText;
 
-// Function to handle button click events
-function addButtonClickListeners() {
-  buttons.forEach((item) => {
-    item.onclick = () => {
-      const buttonText = item.innerText;
+        if (buttonText === 'C') {
+            expression = '';
+        } else if (buttonText === '=') {
+            try {
+                const result = eval(expression);
+                expression = result.toString();
+            } catch (error) {
+                expression = 'Error';
+            }
+        } else if (buttonText === '<') {
+            expression = expression.slice(0, -1);
+        } else {
+            expression += buttonText;
+        }
 
-      switch (buttonText) {
-        case "C":
-          display.innerText = "";
-          break;
-        case "<":
-          display.innerText = display.innerText.slice(0, -1);
-          break;
-        case "=":
-          const result = evaluateExpression(display.innerText);
-          display.innerText = result;
-          break;
-        default:
-          display.innerText += buttonText;
-          break;
-      }
-    };
-  });
-}
+        display.innerText = expression;
+    });
+});
 
-// Theme toggle functionality
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
+const themeToggler = document.querySelector('.theme-toggler');
 
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  calculator.classList.toggle("light");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-
-  if (isDark) {
-    document.body.style.backgroundImage = "url('darkbg.png')";
-  } else {
-    document.body.style.backgroundImage = "url('lightbg.png')";
-  }
-
-  // Call the function to reassign the button click event listeners
-  addButtonClickListeners();
-};
-
-// Call the function initially to assign the button click event listeners
-addButtonClickListeners();
+themeToggler.addEventListener('click', () => {
+    const calculator = document.querySelector('.calculator');
+    calculator.classList.toggle('dark');
+    themeToggler.classList.toggle('active');
+});
