f1b4dd855b160fed94d9c9e0fff00daf7508947d
Grimet14 <gileff.simon@gmail.com>
2023-05-25 00:06:10 -0700

    made both themes transparent, added bg images, input not working

--- script.js
+++ script.js
@@ -1,70 +1,63 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id === "clear") {
-      display.innerText = "";
-    } else if (item.id === "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (item.id === "equal") {
-      if (display.innerText !== "") {
-        display.innerText = eval(display.innerText);
-      } else {
-        display.innerText = "Empty!";
-        setTimeout(() => (display.innerText = ""), 2000);
-      }
-    } else {
-      display.innerText += item.id;
-    }
-  };
-});
-
-// Keyboard event listener
-document.addEventListener("keydown", (event) => {
-  const key = event.key;
-
-  if (/^[0-9]$/.test(key)) {
-    // Number keys
-    display.innerText += key;
-  } else if (key === "+" || key === "-" || key === "*") {
-    // Operator keys (+, -, *)
-    display.innerText += key;
-  } else if (key === "/" || key === "x") {
-    // Division (/) or Multiplication (x)
-    display.innerText += key === "x" ? "*" : "/";
-  } else if ((event.shiftKey && key === "9") || (event.shiftKey && key === "(")) {
-    // Shift+( or Shift+9
-    display.innerText += "(";
-  } else if ((event.shiftKey && key === "0") || (event.shiftKey && key === ")")) {
-    // Shift+) or Shift+0
-    display.innerText += ")";
-  } else if (key === "Enter") {
-    // Equal key
-    event.preventDefault();
-    if (display.innerText !== "") {
-      display.innerText = eval(display.innerText);
-    } else {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    }
-  } else if (key === "Backspace") {
-    // Backspace key
-    let string = display.innerText.toString();
-    display.innerText = string.substr(0, string.length - 1);
-  } else if (key === "c" || key === "C") {
-    // Clear key
-    display.innerText = "";
+// Function to evaluate an arithmetic expression
+function evaluateExpression(expression) {
+  try {
+    const result = new Function(`return ${expression}`)();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return "Error: " + error.message;
   }
-});
+}
 
+// Function to handle button click events
+function addButtonClickListeners() {
+  buttons.forEach((item) => {
+    item.onclick = () => {
+      const buttonText = item.innerText;
+
+      switch (buttonText) {
+        case "C":
+          display.innerText = "";
+          break;
+        case "<":
+          display.innerText = display.innerText.slice(0, -1);
+          break;
+        case "=":
+          const result = evaluateExpression(display.innerText);
+          display.innerText = result;
+          break;
+        default:
+          display.innerText += buttonText;
+          break;
+      }
+    };
+  });
+}
+
+// Theme toggle functionality
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
 let isDark = true;
+
 themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
+  calculator.classList.toggle("light");
   themeToggleBtn.classList.toggle("active");
   isDark = !isDark;
+
+  if (isDark) {
+    document.body.style.backgroundImage = "url('darkbg.png')";
+  } else {
+    document.body.style.backgroundImage = "url('lightbg.png')";
+  }
+
+  // Call the function to reassign the button click event listeners
+  addButtonClickListeners();
 };
+
+// Call the function initially to assign the button click event listeners
+addButtonClickListeners();
